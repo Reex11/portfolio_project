e18accb8063c99037417b70ed9d944abf8096700
Abdulmalik <reex4des@gmail.com>
2023-05-31 00:24:03 +0300

    refactor database connection

--- lib/database.php
+++ lib/database.php
@@ -2,18 +2,11 @@
 
 class Database {
 
-    private static $db_host = $_ENV['DB_HOST'];
-    private static $db_name = $_ENV['DB_NAME'];
-    private static $db_user = $_ENV['DB_USER'];
-    private static $db_pass = $_ENV['DB_PASS'];
-    private static $db_port = $_ENV['DB_PORT'];
-    private $db_connection = null;
-
     public static function getConnection() {
 
-        self::$db_connection = new PDO("mysql:host=" . self::$db_host . ";dbname=" . self::$db_name . ";port=" . self::$db_port, self::$db_user, self::$db_pass);
-        self::$db_connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        return self::$db_connection;
+        $db_connection = new PDO("mysql:host=" . $_ENV['DB_HOST'] . ";dbname=" . $_ENV['DB_NAME'] . ";port=" . $_ENV['DB_PORT'], $_ENV['DB_USER'], $_ENV['DB_PASS']);
+        $db_connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $db_connection;
     }
 
 }
